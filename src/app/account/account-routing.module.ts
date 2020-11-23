e55178bf11f6b6ce15034a82bbb7fdf55b1f8b90
procodervikas <vikaskpro98@gmail.com>
2020-11-23 19:06:25 +0530

    work till 7:06 on 23 nov

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component'
 
-
+const routes: Routes = [
+  // {
+  //   component: LoginComponent,
+  //   path: "login"
+  // },
+  // {
+  //   component:RegisterComponent,
+  //   path: "register"
+  // },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'login'
+  // }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AccountRoutingModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
-import { AdminComponent } from './account/admin/admin.component';
-import { AuthComponent} from './account/auth/auth.component';
+// import { AdminComponent } from './account/admin/admin.component';
+import { RegisterComponent} from './account/register/register.component'
+// import { AuthComponent} from './account/auth/auth.component';
 
 const routes: Routes = [
   {
@@ -10,8 +11,8 @@ const routes: Routes = [
     path: "login"
   },
   {
-    path: 'admin',
-    component: AdminComponent
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: '',
